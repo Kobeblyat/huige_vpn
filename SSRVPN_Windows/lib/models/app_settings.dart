e0fb59126cad0bf3415178f20a98ea62cc6d078c
Glaroday <glaroday@users.noreply.github.com>
2026-08-23 19:47:40 +0800

    chore: 仅保留官方发布说明与 Release 分发

--- SSRVPN_Windows/lib/models/app_settings.dart
+++
@@ -1,5 +0,0 @@
-library;
-
-/// 此文件作为兼容性 re-export，不定义新类。
-
-export 'package:ssrvpn_shared/ssrvpn_shared.dart' show AppSettings, ProxyMode;
--- SSRVPN_Windows/lib/widgets/glass_container.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: unnecessary_library_name
-
-library desktop_glass_container;
-
-import 'dart:ui';
-import 'package:flutter/material.dart';
-import '../theme/app_theme.dart';
-
-part 'package:ssrvpn_shared/desktop_ui/widgets/glass_container_part.dart';
